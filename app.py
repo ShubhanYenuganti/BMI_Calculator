15338a11a4847d3c89f7bf2864c84aaee663a808
Shubhan Yenuganti <yrshubhan@gmail.com>
2022-05-28 16:35:02 -0700

    Updated app.py

--- app.py
+++ app.py
@@ -7,23 +7,21 @@ def rootpage():
     weight = ''
     height = ''
     bmi = ''
-    format_bmi = ''
     health = ''
     if request.method == 'POST' and 'weight' in request.form and 'height' in request.form:
         weight = int(request.form.get('weight'))
         height = float(request.form.get('height'))
-        bmi = weight / (height * height);
-        format_bmi = float("{:.2f}".format(bmi))
+        bmi = float("{:.2f}".format(weight / (height * height)));
 
-        if (format_bmi < 18.5):
+        if (bmi < 18.5):
             health = 'underweight'
-        elif (format_bmi >= 18.5 and format_bmi <= 24.9):
+        elif (bmi >= 18.5 and bmi <= 24.9):
             health = 'normal weight'
-        elif (format_bmi >= 25 and format_bmi <= 29.9):
+        elif (bmi >= 25 and bmi <= 29.9):
             health = 'overweight'
         else:
             health = 'obese'
     
-    return render_template('index.html', format_bmi = format_bmi, health = health)
+    return render_template('index.html', format_bmi = bmi, health = health)
 
 app.run(port = 7777)
